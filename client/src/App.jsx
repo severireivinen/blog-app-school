a8006c712409d1f6358467e1eee9c1b5ac04f824
nicalx <c3eo18@gmail.com>
2022-03-04 17:32:31 +0200

    Modal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.scss";
+import BlogModal from "./components/BlogModal/BlogModal";
 import NewBlogButton from "./components/NewBlogButton/NewBlogButton";
 import { Blogs } from "./containers/Blogs/Blogs";
 
 const App = () => {
+  
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  }
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="app">
       <Blogs />
-      <NewBlogButton />
+      <NewBlogButton openModal={openModal}/>
+      <div>
+        {modalIsOpen && <BlogModal closeModal={closeModal} modalIsOpen={modalIsOpen}/>}
+      </div>
     </div>
   );
 };
