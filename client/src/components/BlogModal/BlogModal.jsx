fd4b811a171360e9db98ab9ade3c9cf6313d8b2d
nicalx <c3eo18@gmail.com>
2022-03-07 18:17:23 +0200

    fix

--- client/src/components/BlogModal/BlogModal.jsx
+++ client/src/components/BlogModal/BlogModal.jsx
@@ -1,46 +1,6 @@
 import React from "react";
 import Modal from "react-modal";
 
-<<<<<<< HEAD
-import modalStyles from "./BlogModal.scss";
-/*
-const modalStyles = {
-    content: {
-        top: '35%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        width: '60%',
-        transform: 'translate(-40%, -10%)',
-        
-    }
-}
-*/
-Modal.setAppElement("#root");
-const BlogModal = ({ modalIsOpen, closeModal }) => {
-  return (
-    <div className="">
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        //style={modalStyles}
-      >
-        <h1>Write a new Blog</h1>
-        <form>
-          <input type="text" name="header" />
-          <textarea type="text" name="content" />
-          <button type="submit" value="Post Blog">
-            Post Blog
-          </button>
-
-          <button onClick={closeModal}>Close</button>
-        </form>
-      </Modal>
-    </div>
-  );
-};
-=======
 import "./BlogModal.scss";
 Modal.setAppElement('#root')
 const BlogModal = ({modalIsOpen, closeModal}) => {
@@ -70,6 +30,5 @@ const BlogModal = ({modalIsOpen, closeModal}) => {
         
   )
 }
->>>>>>> 08338f0d34155ebca572783e5954cdb1a6235253
 
 export default BlogModal;
