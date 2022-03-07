c98443d53209dc8d3969f378aec46ea841142c82
Severi Reivinen <severire@metropolia.fi>
2022-03-07 19:13:25 +0200

    author added to blogs

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -4,10 +4,15 @@ import { GoThumbsup } from "react-icons/go";
 import "./Blog.scss";
 
 const Blog = ({ blog }) => {
+  console.log(blog);
+
   return (
     <div className="app__blog">
       <div className="app__blog-header">
-        <h2 className="head-text">{blog.title}</h2>
+        <div className="app__blog-hero">
+          <h2 className="head-text">{blog.title}</h2>
+          <span>{blog.user.username}</span>
+        </div>
         <div>
           <p className="">{blog.likes}</p>
           <GoThumbsup />
